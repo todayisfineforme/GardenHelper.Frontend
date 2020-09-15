54856e66e9b92f3cee3bc381e1e7cb44056d9ad1
melanie <melanieduah@gmail.com>
2020-09-15 14:38:47 -0500

    Updates

---
+++ precache-manifest.f455ed93569adf55d276981aaa5f7590.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7053b8856a89c59a411df5264c62836b",
+    "url": "/Project-3-Frontend.github.io/garden-helper/index.html"
+  },
+  {
+    "revision": "df7c8af8b5f4317b31e2",
+    "url": "/Project-3-Frontend.github.io/garden-helper/static/css/main.649c8898.chunk.css"
+  },
+  {
+    "revision": "436f6f03132a462e6e8d",
+    "url": "/Project-3-Frontend.github.io/garden-helper/static/js/2.6ca77f4a.chunk.js"
+  },
+  {
+    "revision": "194dcbc93009dd3a264c2ecd82a23a4f",
+    "url": "/Project-3-Frontend.github.io/garden-helper/static/js/2.6ca77f4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df7c8af8b5f4317b31e2",
+    "url": "/Project-3-Frontend.github.io/garden-helper/static/js/main.9740123a.chunk.js"
+  },
+  {
+    "revision": "e32e4100b02a5051bcc4",
+    "url": "/Project-3-Frontend.github.io/garden-helper/static/js/runtime-main.313bbb29.js"
+  },
+  {
+    "revision": "431eed1512055aa6b0d323b92a89f258",
+    "url": "/Project-3-Frontend.github.io/garden-helper/static/media/hero.431eed15.jpg"
+  },
+  {
+    "revision": "1e71e9846f5f223968925bc78c0f4b58",
+    "url": "/Project-3-Frontend.github.io/garden-helper/static/media/hero2.1e71e984.jpg"
+  }
+]);
